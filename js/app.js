b7ebdfc5b026203c211f3a6ca0a28ea406bd9191
BK <bkam03@yahoo.com>
2017-06-10 12:30:05 -1000

    date difference calculation function created

--- js/app.js
+++ js/app.js
@@ -14,23 +14,27 @@ function generateContent (){
       var postThumbnail = document.createElement( 'div' );
       postThumbnail.className = 'post';
 
-      var image = createPostContent( onePost.thumbnail, 'img', 'src', 'thumbnail', postThumbnail );
+      var image = createPostContent( 'img', 'thumbnail' );
+      image.src = onePost.thumbnail;
       postThumbnail.appendChild( image );
 
-      var title = createPostContent( onePost.title, 'p', 'innerHTML', '"postDescription", "title"', postThumbnail );
+      var title = createPostContent( 'p', 'title' );
+      title.innerHTML = onePost.title;
       postThumbnail.appendChild( title );
 
-      var author = createPostContent( onePost.author, 'p', 'innerHTML', '"postDescription", "author"', postThumbnail );
-      postThumbnail.appendChild( author );
+      var metaData = document.createElement( 'div' );
+      metaData.className = 'metaData';
+      var dateCreated = new Date( onePost.created * 1000 );
+      var dateNow = new Date();
+      var difference = convertUnixTimeStamp( dateNow - dateCreated );
+      metaData.innerHTML = "by " + onePost.author + " @ " + " Posted " +difference + " @ " + onePost.viewcount;
+      console.log('timecalc', dateCreated, '\n', dateNow, '\n', difference);
 
-      var dateCreated = createPostContent( onePost.created, 'p', 'innerHTML', '"postDescription", "dateCreated"', postThumbnail );
-      var date = new Date( onePost.created * 1000 );
-      console.log( typeof date, date );
-      dateCreated.innerHTML = date;
-      postThumbnail.appendChild( dateCreated );
+      var summary = createPostContent( 'p', 'summary' );
+      summary.innerHTML = onePost.selftext;
+      postThumbnail.appendChild( summary );
 
-      var viewcount = createPostContent( onePost.viewcount, 'p', 'innerHTML', '"postDescription", "viewcount"',  postThumbnail );
-      postThumbnail.appendChild( dateCreated );
+      postThumbnail.appendChild( metaData );
 
       var targetGrid = document.getElementById( 'grid' );
       targetGrid.appendChild( postThumbnail );
@@ -43,14 +47,38 @@ function generateContent (){
   targetGrid.appendChild( createContentContainer );
 }
 
-function createPostContent ( contentSource, elementType, attributeType, className, appendTarget ){
+function createPostContent ( elementType, className ){
   var newElement = document.createElement( elementType );
-  newElement[attributeType] = contentSource;
   newElement.className = className;
   return newElement;
-/*  appendTarget.appendChild( newElement );
-*/}
+}
 
+function convertUnixTimeStamp ( timeStamp ){
+  var yearInSeconds = 31557600;
+  var monthInSeconds = 2392000;
+  var weekInSeconds = 604800;
+  var dayInSeconds = 86400;
+  var hourInSeconds = 3600;
+  var minuteInSeconds = 60;
+
+  var timeElapsed = 0;
+
+  if( timeStamp / yearInSeconds >= 1 ){
+    timeElapsed = timeStamp / yearInSeconds + ' years ago';
+  } else if( timeStamp / monthInSeconds >= 1 ){
+    timeElapsed = timeStamp / monthInSeconds + ' months ago';
+  } else if( timeStamp / weekInSeconds >= 1 ){
+    timeElapsed = timeStamp / weekInSeconds + ' weeks ago';
+  } else if( timeStamp / dayInSeconds >= 1 ){
+    timeElapsed = timeStamp / dayInSeconds + ' days ago';
+  } else if( timeStamp / hourInSeconds >= 1 ){
+    timeElapsed = timeStamp / hourInSeconds + ' hours ago';
+  } else {
+    timeElapsed = timeStamp / minuteInSeconds + ' minutes ago';
+  }
+
+  return timeElapsed;
+}
 
 var menuItemArray = document.getElementsByClassName( 'menuItem' );
 menuItemArray[1].addEventListener( 'click', generateContent );
