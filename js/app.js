463895ac6d9ba5e902e28b34230552f2043d0898
BK <bkam03@yahoo.com>
2017-06-10 10:28:47 -1000

    basic thumbnails, unformatted are generated when myBoards is clicked

--- js/app.js
+++ js/app.js
@@ -35,15 +35,19 @@ function generateContent (){
     var arrayOfPosts = response.data .children;
 
     for( var i = 0; i < 4; i++ ){
-      //isolates data from each post
       var onePost = arrayOfPosts[i].data;
       console.log( onePost.title, onePost.author, onePost.viewcount );
 
-      //creates and appends preview image
-      var image = onePost.thumbnail;
-      var testImage = document.createElement( 'img' );
-      testImage.src = image;
-      createContentContainer.appendChild( testImage );
+      var postThumbnail = document.createElement( 'div' );
+      postThumbnail.className = 'post';
+
+      createPostContent( onePost.thumbnail, 'img', 'src', postThumbnail );
+      createPostContent( onePost.title, 'p', 'innerHTML', postThumbnail );
+      createPostContent( onePost.author, 'p', 'innerHTML', postThumbnail );
+      createPostContent( onePost.viewcount, 'p', 'innerHTML', postThumbnail );
+
+      var targetGrid = document.getElementById( 'grid' );
+      targetGrid.appendChild( postThumbnail );
     }
   });
   newRequest.open( 'GET', 'https://www.reddit.com/r/NatureIsFuckingLit.json' );
@@ -51,6 +55,12 @@ function generateContent (){
 
   var targetGrid = document.getElementById( 'grid' );
   targetGrid.appendChild( createContentContainer );
+}
+
+function createPostContent ( contentSource, elementType, attributeType, appendTarget ){
+  var newElement = document.createElement( elementType );
+  newElement[attributeType] = contentSource;
+  appendTarget.appendChild( newElement );
 }
 
 
