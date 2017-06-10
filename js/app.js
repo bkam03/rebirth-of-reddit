9be0a94bc5f9296aafde9e12a77f6df925d0448b
BK <bkam03@yahoo.com>
2017-06-10 11:08:34 -1000

    removed post limit, will load what is on page

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-/*
-//requests posts from reddit
-var newRequest = new XMLHttpRequest();
-newRequest.addEventListener( 'load', function (){
-  var response = JSON.parse( this.responseText );
-  var arrayOfPosts = response.data .children;
-
-  //isolates data from each post
-  var onePost = arrayOfPosts[0].data;
-  console.log( onePost.title, onePost.author, onePost.viewcount );
-
-  //creates and appends preview image
-  var image = onePost.thumbnail;
-  var testImage = document.createElement( 'img' );
-  testImage.src = image;
-  console.log( testImage );
-  document.getElementById( 'grid' ).appendChild( testImage );
-} );
-newRequest.open( 'GET', 'https://www.reddit.com/r/NatureIsFuckingLit.json' );
-newRequest.send();
-
-function setDataOfElement( element, sourceUrl ){
-  element.data.sourceUrl = sourceUrl;
-}*/
-
-
 function generateContent (){
   var createContentContainer = document.createElement( 'div' );
   createContentContainer.className = 'contentContainer';
@@ -34,17 +8,17 @@ function generateContent (){
     var response = JSON.parse( this.responseText );
     var arrayOfPosts = response.data .children;
 
-    for( var i = 0; i < 4; i++ ){
+    for( var i = 0; i < arrayOfPosts.length; i++ ){
       var onePost = arrayOfPosts[i].data;
       console.log( onePost.title, onePost.author, onePost.viewcount );
 
       var postThumbnail = document.createElement( 'div' );
       postThumbnail.className = 'post';
 
-      createPostContent( onePost.thumbnail, 'img', 'src', postThumbnail );
-      createPostContent( onePost.title, 'p', 'innerHTML', postThumbnail );
-      createPostContent( onePost.author, 'p', 'innerHTML', postThumbnail );
-      createPostContent( onePost.viewcount, 'p', 'innerHTML', postThumbnail );
+      createPostContent( onePost.thumbnail, 'img', 'src', 'thumbnail', postThumbnail );
+      createPostContent( onePost.title, 'p', 'innerHTML', 'postDescription', postThumbnail );
+      createPostContent( onePost.author, 'p', 'innerHTML', 'postDescription', postThumbnail );
+      createPostContent( onePost.viewcount, 'p', 'innerHTML', 'postDescription',  postThumbnail );
 
       var targetGrid = document.getElementById( 'grid' );
       targetGrid.appendChild( postThumbnail );
@@ -57,15 +31,14 @@ function generateContent (){
   targetGrid.appendChild( createContentContainer );
 }
 
-function createPostContent ( contentSource, elementType, attributeType, appendTarget ){
+function createPostContent ( contentSource, elementType, attributeType, className, appendTarget ){
   var newElement = document.createElement( elementType );
   newElement[attributeType] = contentSource;
+  newElement.className = className;
   appendTarget.appendChild( newElement );
 }
 
 
 var menuItemArray = document.getElementsByClassName( 'menuItem' );
-
-
 menuItemArray[1].addEventListener( 'click', generateContent );
 
