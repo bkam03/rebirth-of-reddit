08c7f06be3dcf2f154b39921f89fb80d08fa15c9
BK <bkam03@yahoo.com>
2017-06-10 10:13:07 -1000

    basic container of 4 images appears on page when myBoards is clicked

--- js/app.js
+++ js/app.js
@@ -1,25 +1,61 @@
-
+/*
 //requests posts from reddit
-var test = new XMLHttpRequest();
-test.addEventListener( 'load', function (){
-  //console.log( JSON.parse( this.responseText ) );
+var newRequest = new XMLHttpRequest();
+newRequest.addEventListener( 'load', function (){
   var response = JSON.parse( this.responseText );
   var arrayOfPosts = response.data .children;
 
   //isolates data from each post
   var onePost = arrayOfPosts[0].data;
   console.log( onePost.title, onePost.author, onePost.viewcount );
-  var image = onePost.secure_media.oembed.thumbnail_url;
-
 
   //creates and appends preview image
+  var image = onePost.thumbnail;
   var testImage = document.createElement( 'img' );
   testImage.src = image;
   console.log( testImage );
   document.getElementById( 'grid' ).appendChild( testImage );
-
-
-
 } );
-test.open( 'GET', 'https://www.reddit.com/r/SlowMotion.json' );
-test.send();
+newRequest.open( 'GET', 'https://www.reddit.com/r/NatureIsFuckingLit.json' );
+newRequest.send();
+
+function setDataOfElement( element, sourceUrl ){
+  element.data.sourceUrl = sourceUrl;
+}*/
+
+
+function generateContent (){
+  var createContentContainer = document.createElement( 'div' );
+  createContentContainer.className = 'contentContainer';
+
+  //requests posts from reddit
+  var newRequest = new XMLHttpRequest();
+  newRequest.addEventListener( 'load', function (){
+    var response = JSON.parse( this.responseText );
+    var arrayOfPosts = response.data .children;
+
+    for( var i = 0; i < 4; i++ ){
+      //isolates data from each post
+      var onePost = arrayOfPosts[i].data;
+      console.log( onePost.title, onePost.author, onePost.viewcount );
+
+      //creates and appends preview image
+      var image = onePost.thumbnail;
+      var testImage = document.createElement( 'img' );
+      testImage.src = image;
+      createContentContainer.appendChild( testImage );
+    }
+  });
+  newRequest.open( 'GET', 'https://www.reddit.com/r/NatureIsFuckingLit.json' );
+  newRequest.send();
+
+  var targetGrid = document.getElementById( 'grid' );
+  targetGrid.appendChild( createContentContainer );
+}
+
+
+var menuItemArray = document.getElementsByClassName( 'menuItem' );
+
+
+menuItemArray[1].addEventListener( 'click', generateContent );
+
